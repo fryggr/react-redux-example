1302146046b3a7dc51d7cf5c90501a6b21ab6896
Daria Sminova <daria@realrussialimited.local>
2019-01-22 12:51:07 +0400

    first commit

---
+++ src/reducers/items.js
@@ -0,0 +1,35 @@
+import {
+    ITEMS_HAS_ERRORED,
+    ITEMS_IS_LOADING,
+    ITEMS_FETCH_DATA_SUCCESS
+} from '../constants'
+
+export const itemsHasErrored = (state = false, action) => {
+    switch (action.type) {
+        case ITEMS_HAS_ERRORED:
+            return action.hasErrored;
+
+        default:
+            return state;
+    }
+}
+
+export const itemsIsLoading = (state = false, action) => {
+    switch (action.type) {
+        case ITEMS_IS_LOADING:
+            return action.isLoading;
+
+        default:
+            return state;
+    }
+}
+
+export const items = (state = [], action) => {
+    switch (action.type) {
+        case ITEMS_FETCH_DATA_SUCCESS:
+            return action.items;
+
+        default:
+            return state;
+    }
+}
